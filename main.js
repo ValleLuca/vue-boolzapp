e160392d5bdfabde65abef5b5e9e4246dede098d
ValleLuca <valle.lucaAb@outlook.it>
2022-01-18 21:42:58 +0100

    Dom vagamente presente

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        datiUtentiChat: [
+          {
+            utente: 'Alfredo Riso',
+            avatar: 'img/avatar_1.png',
+            data: '18/05/2021',
+            chatUno: 'mi sembra più che giusto',
+            chatDue: 'Vediamo che sai fare',
+            rispostaUno : 'perché?',
+            rispostaDue: 'non mi fai paura'
+          }
+        ]
     }
   })
