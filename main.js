f30834cf682b42466805722e866f988c20fdbfa4
ValleLuca <valle.lucaAb@outlook.it>
2022-01-19 15:26:05 +0100

    Aggiunte immagini, e modificato e aggiunto un for

--- main.js
+++ main.js
@@ -3,14 +3,71 @@ var app = new Vue({
     data: {
         datiUtentiChat: [
           {
-            utente: 'Alfredo Riso',
-            avatar: 'img/avatar_1.png',
-            data: '18/05/2021',
-            chatUno: 'mi sembra più che giusto',
-            chatDue: 'Vediamo che sai fare',
-            rispostaUno : 'perché?',
-            rispostaDue: 'non mi fai paura'
-          }
+            name: 'Michele',
+            avatar: '_1',
+            visible: true,
+            messages: [
+              {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+              }
+            ],
+          },
+          {
+            name: 'Flavio',
+            avatar: '_1',
+            visible: true,
+            messages: [
+              {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+              }
+            ],
+          },
+          {
+            name: 'Bruno',
+            avatar: '_1',
+            visible: true,
+            messages: [
+              {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+              }
+            ],
+          },
         ]
     }
   })
